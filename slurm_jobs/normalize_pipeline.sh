1deab9ebefddab57a0d732a877975addb77ada0b
Benjamin Lyo <bl3021@log-1.hpc.nyu.edu>
2022-01-28 14:50:10 -0500

    reorganized files into folders and modified slurm files for working on NYU cluster

--- slurm_jobs/normalize_pipeline.sh
+++ slurm_jobs/normalize_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PARAM_FILE="mnist_param.txt"
 
-SAVE_FOLDER="./resNet"
+SAVE_FOLDER="/scratch/bl3021/research/sy-lab/robust-normalization/resNet/"
 MODEL_NAME=("standard") #mode of the model, can be "neural_noise", "standard" or "gaussian"
 SEEDS=("17")
 MODES=("val") #mode can be 'train', 'val', 'extract', 'mft', 'pairwise', 'meanvar'
@@ -12,6 +12,7 @@ WEIGHT_DECAYS=("0.0005")
 NORMALIZES=("lrnb")
 #EPS=("0.01_0.03_0.05_0.07_0.1_0.15_0.2")
 EPS=("1.0_2.0_4.0_6.0_8.0")
+EPS=("1.0")
 
 > ${PARAM_FILE}
 for MODEL in ${MODEL_NAME[@]};do
@@ -34,7 +35,7 @@ done
 
 # 2.a Iterate through parameter lines.
 # For every 100 lines we should create a new job array
-
+echo "2a Iterate through parameter lines"
 LINE_COUNT=0
 START_INDEX=1
 ARRAY_INDEX=0
@@ -44,7 +45,7 @@ while read line; do
         if [ "$(expr ${LINE_COUNT} % 100)" = "0" ]
         then
                 echo "New Array For Parameters from ${START_INDEX} to ${LINE_COUNT}"
-                sbatch --array=1-100 --mem 16G -p normal normalize.sbatch ${ARRAY_INDEX} ${PARAM_FILE}
+                sbatch --array=1-100 normalize.sbatch ${ARRAY_INDEX} ${PARAM_FILE}
                 START_INDEX=$(expr ${LINE_COUNT} + 1)
                 ARRAY_INDEX=$(expr ${ARRAY_INDEX} + 1)
         fi
@@ -52,10 +53,14 @@ while read line; do
 done < ${PARAM_FILE}
 
 # 2.b Check to make sure that all parameters were tested
+echo "2b Check to make sure that all parameters were tested"
 if [ "${LINE_COUNT}" -ge "${START_INDEX}" ]
 then
+	echo ${LINE_COUNT}
+	echo ${START_INDEX}
         DIFF=$(expr ${LINE_COUNT} - ${START_INDEX} + 1)
         echo "New Array For Parameters from ${START_INDEX} to ${LINE_COUNT}"
-        sbatch --array=1-${DIFF} --mem 16G -p normal normalize.sbatch ${ARRAY_INDEX} ${PARAM_FILE}
+        #sbatch --array=1-${DIFF} --mem 16G normalize.sbatch ${ARRAY_INDEX} ${PARAM_FILE}
+	sbatch --array=1-${DIFF} normalize.sbatch ${ARRAY_INDEX} ${PARAM_FILE}
 fi
 

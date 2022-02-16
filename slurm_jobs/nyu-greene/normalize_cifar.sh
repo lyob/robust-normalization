239b6da5597d19eb244ae59cfff22c04b75be33a
Ben Lyo <blyo@choux-choux>
2022-02-16 12:28:04 -0500

    split greene slurm files to cifar and mnist versions

---
+++ slurm_jobs/nyu-greene/normalize_cifar.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SAVE_FOLDER=$1
+MODEL=$2
+SEED=$3
+MODE=$4
+LEARNING_RATE=$5
+WEIGHT_DECAY=$6
+NORMALIZE=$7
+EP=$8
+
+module purge;
+bash /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+cd /scratch/bl3021/research/sy-lab/;
+conda activate ./penv;
+
+cd robust-normalization/code/
+
+python3 cifar_normalize.py \
+	--save_folder ${SAVE_FOLDER} \
+	--model_name ${MODEL} \
+	--seed ${SEED} \
+	--learning_rate ${LEARNING_RATE} \
+	--weight_decay ${WEIGHT_DECAY} \
+	--mode ${MODE} \
+	--eps ${EP} \
+	--normalize ${NORMALIZE} \
+

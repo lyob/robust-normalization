2eace0dd076847967e697698bcfbadd15facb06f
Benjamin Lyo <bl3021@log-2.hpc.nyu.edu>
2022-02-21 14:25:19 -0500

    mftma rename

--- slurm_jobs/nyu-greene/mftma/mftma.sh
+++ slurm_jobs/nyu-greene/mftma/mftma.sh
@@ -7,13 +7,14 @@ NORM_METHOD=$4
 
 
 module purge;
-bash /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
-cd /scratch/bl3021/research/sy-lab/;
-conda activate pyenv36;
+source /share/apps/anaconda3/2020.07/etc/profile.d/conda.sh;
+# conda activate pyenv36;
+conda activate /scratch/bl3021/.conda/envs/pyenv36
 
+cd /scratch/bl3021/research/sy-lab/;
 cd robust-normalization/code/analysis/
 
-python3 parallel_mftma.py \
+python -m parallel_mftma \
 	--save_folder ${SAVE_FOLDER} \
 	--dataset_name ${DATASET}
 	--seed ${SEED} \

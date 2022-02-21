b1b4fe841a0878fc7b710a97262dc1c2987156e1
Ben Lyo <blyo@choux-choux>
2022-02-20 21:25:47 -0500

    analysis code now works for cifar dataset

--- code/analysis/mftma_analysis.py
+++ code/analysis/mftma_analysis.py
@@ -8,9 +8,8 @@ import sys
 folder_path = '../'
 os.chdir(folder_path)
 
-from cifar_layer_norm import ResNet
+from cifar_layer_norm import ResNet, BasicBlock
 from mnist_layer_norm import Net
-
 
 import matplotlib.pyplot as plt
 import torch
@@ -21,40 +20,77 @@ from importlib import reload
 import mftma
 reload(mftma)
 from mftma.manifold_analysis_correlation import manifold_analysis_corr
-
 from mftma.utils.analyze_pytorch import analyze
 from mftma.utils.activation_extractor import extractor
-from art.utils import load_mnist, load_cifar10
 
+from art.utils import load_mnist, load_cifar10
+from robustness import model_utils, attacker
+from robustness.datasets import CIFAR
+import dill
 
 #%% import trained model and select normalization method
+
+dataset = "cifar"  # cifar or mnist
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 # import trained models
-def import_trained_model(m="nn"):
-    model_name_base = "standard-lr_0.01-wd_0.0005-seed_17-normalize_"
-    save_folder = os.path.join("..", "results", "mnist_regularize", "trained_models", "standard")
+def import_trained_model(n="nn"):
+    save_folder = os.path.join("..", "results", f"{dataset}_regularize", "trained_models", "standard")
+    if (dataset=="mnist"):
+        model_name_base = "standard-lr_0.01-wd_0.0005-seed_17-normalize_"
+        model_name = os.path.join(save_folder, f"{model_name_base}{n}.pth")
+        print(model_name)
 
-    # import the model from code file
-    simple_channels = 16
-    complex_channels = 16
-    ksize = 5
-    conv_1 = nn.Conv2d(in_channels=1, out_channels=simple_channels+complex_channels, kernel_size=ksize, stride=2, padding=ksize//2)
-    model = Net(conv_1, simple_channels + complex_channels, normalize=m)
-    model = model.to(device)
+        # load model
+        simple_channels = 16
+        complex_channels = 16
+        ksize = 5
+        conv_1 = nn.Conv2d(in_channels=1, out_channels=simple_channels+complex_channels, kernel_size=ksize, stride=2, padding=ksize//2)
+        model = Net(conv_1, simple_channels + complex_channels, normalize=n)
+        model = model.to(device)
 
-    model_name = os.path.join(save_folder, f"{model_name_base}{m}.pth")
-    print(model_name)
-    # print(model)
-    model.load_state_dict(torch.load(model_name, map_location=device))
+        sd = torch.load(model_name, map_location=device)
+        print('sd', sd)
+        model.load_state_dict(sd)
+        model.eval()
+    
+    if (dataset=="cifar"):
+        model_name_folder = f"standard-normalize_{n}-wd_0.0005-seed_17"
+        model_name = os.path.join(save_folder, model_name_folder, "checkpoint.pt.best")
+
+        # define architecture
+        arch = ResNet(BasicBlock, [2,2,2,2], normalize=n)
+
+        # load dataset
+        ds = CIFAR(data_path='../../datasests/')
+        
+        # load previously saved model
+        model = attacker.AttackerModel(arch, ds)
+        # model, _ = model_utils.make_and_restore_model(arch=model, dataset=ds,
+        #     resume_path=model_name)
+        # model = model.model
+
+        # load saved weights
+        checkpoint = torch.load(model_name, pickle_module=dill, map_location=device)
+        
+        sd = checkpoint["model"]
+        sd = {k[len('module.'):]:v for k,v in sd.items()}
+        model.load_state_dict(sd)
+        model.eval()
+        # model = model.to(device)
+        print("=> loaded checkpoint '{}' (epoch {})".format(model_name, checkpoint['epoch']))
+
     return model
 
 # for idx, m in enumerate(normalize_method):
 trained_models = {}
 normalize_method = ["bn", "gn", "in", "ln", "lrnb", "lrnc", "lrns", "nn"]
-model = import_trained_model("nn")
+models = dict()
+for idx, m in enumerate(normalize_method):
+    model = import_trained_model(m)
+    models[m] = model
 
 #%% create manifold dataset and extract activations
 
@@ -79,11 +115,11 @@ def create_manifold_dataset(model, dataset="mnist"):
         train_dataset = (x_train, y_train)
     elif dataset=="cifar":
         (x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = load_cifar10()
+        x_train = x_train.transpose(0,3,1,2).astype(np.float32)
         train_dataset = (x_train, y_train)
     
     # transpose dataset from tuple of arrays into array of tuples
     train_dataset = list(zip(x_train, y_train))
-
     # print(train_dataset[0][0].shape)
 
     data = make_manifold_data(train_dataset, sampled_classes, examples_per_class, seed=0)
@@ -134,26 +170,40 @@ radii = []
 dimensions = []
 correlations = []
 
+capacities_std = []
+radii_std = []
+dimensions_std = []
+
 for layer_name, X, in activations.items():
     # Analyze each layer's activations
     a, r, d, r0, K = manifold_analysis_corr(X, 0, 300, n_reps=1)
     
     # Compute the mean values
-    a = 1/np.mean(1/a)
-    r = np.mean(r)
-    d = np.mean(d)
+    a_mean = 1/np.mean(1/a)
+    r_mean = np.mean(r)
+    d_mean = np.mean(d)
+
+    # compute the std values
+    a_std = 
+    r_std = np.std(r)
+    d_std = np.std(d)
+    
     print(f"{layer_name}\n \
-        capacity: {a:4f}\n \
-        radius: {r:4f}\n \
-        dimension: {d:4f}\n \
+        capacity: {a_mean:4f} ± {a_std:4f}\n \
+        radius: {r_mean:4f} ± {r_std:4f}\n \
+        dimension: {d_mean:4f} ± {d_std:4f}\n \
         correlation: {r0:4f}"
     )
     
     # Store for later
-    capacities.append(a)
-    radii.append(r)
-    dimensions.append(d)
+    capacities.append(a_mean)
+    radii.append(r_mean)
+    dimensions.append(d_mean)
     correlations.append(r0)
+
+    capacities_std.append(a_std)
+    radii_std.append(r_std)
+    dimensions_std.append(d_std)
 
 
 #%% plot the results

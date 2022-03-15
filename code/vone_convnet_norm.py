implemented normalization in the 1st v1 layer and fixed reproducibility issue

code/vone_convnet_norm.py
@@ -21,14 +21,17 @@ from vonenet.vonenet import VOneNet
 folder_path = '../results'
 os.chdir(folder_path)
     
-def seed_everything(seed):
+def seed_everything(seed: int):
+    import random
     #initiate seed to try to make the result reproducible 
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    #torch.backends.cudnn.deterministic = True
-    #torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
     
 def calculate_norm(model):
     norm_dict = {}
@@ -43,9 +46,8 @@ def calculate_norm(model):
                 norm_dict[name] = np.sum(np.sqrt(np.sum(param**2, axis=1)))
     return norm_dict
 
-def main(save_folder, model_name, seed, lr, wd, mode, eps, normalize):
-    print(f'Save folder: {save_folder}, model_name: {model_name}, seed: {seed}, mode: {mode}, lr: {lr}, wd: {wd}, normalize: {normalize}', flush=True)
-    seed_everything(seed)
+def main(save_folder, model_name, seed, lr, wd, mode, eps, norm_method):
+    print(f'Save folder: {save_folder}, model_name: {model_name}, seed: {seed}, mode: {mode}, lr: {lr}, wd: {wd}, norm_method: {norm_method}', flush=True)
     #load and process data
     (x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = load_mnist()
 
@@ -58,10 +60,10 @@ def main(save_folder, model_name, seed, lr, wd, mode, eps, normalize):
     if model_name == 'standard':
         conv_1 = nn.Conv2d(in_channels=1, out_channels=simple_channels+complex_channels, 
         kernel_size=ksize, stride=2, padding=ksize//2)
-        model = Net(conv_1, simple_channels + complex_channels, normalize=normalize)
+        model = Net(conv_1, simple_channels + complex_channels, normalize=norm_method)
 
     if model_name == 'vone_convnet-layer1_norm':
-        model = VOneNet(simple_channels=simple_channels, complex_channels=complex_channels, normalize=normalize)
+        model = VOneNet(simple_channels=simple_channels, complex_channels=complex_channels, norm_method=norm_method)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
@@ -87,7 +89,7 @@ def main(save_folder, model_name, seed, lr, wd, mode, eps, normalize):
         save_path = os.path.join(save_folder, 'trained_models', model_name)
         if not os.path.exists(save_path):
             os.makedirs(save_path, exist_ok=True)
-        save_name = os.path.join(save_path, f'{model_name}-lr_{str(lr)}-wd_{str(wd)}-seed_{str(seed)}-normalize_{normalize}.pth')
+        save_name = os.path.join(save_path, f'{model_name}-lr_{str(lr)}-wd_{str(wd)}-seed_{str(seed)}-normalize_{norm_method}.pth')
         torch.save(classifier.model.state_dict(),save_name)
         record = {}
         record['accuracy'] = accuracy
@@ -99,7 +101,7 @@ def main(save_folder, model_name, seed, lr, wd, mode, eps, normalize):
     if mode == 'val':
         eps = [float(i) for i in eps]
         save_path = os.path.join(save_folder, 'trained_models', model_name)
-        save_name = os.path.join(save_path, f'{model_name}-lr_{str(lr)}-wd_{str(wd)}-seed_{str(seed)}-normalize_{normalize}.pth')
+        save_name = os.path.join(save_path, f'{model_name}-lr_{str(lr)}-wd_{str(wd)}-seed_{str(seed)}-normalize_{norm_method}.pth')
         model.load_state_dict(torch.load(save_name))
         model.eval()
         
@@ -140,7 +142,7 @@ def main(save_folder, model_name, seed, lr, wd, mode, eps, normalize):
         if not os.path.exists(save_path_eval):
             os.makedirs(save_path_eval, exist_ok=True)
         save_name_eval = os.path.join(save_path_eval, model_name + '-lr_' + str(lr) + '-wd_' + str(wd) + '-seed_' + str(seed) + 
-        '-normalize_' + normalize + '-eps_' + '_'.join(eps) + '.pkl')
+        '-normalize_' + norm_method + '-eps_' + '_'.join(eps) + '.pkl')
         
         to_save['perturbed'] = record
         save_file = open(save_name_eval, 'wb')
@@ -156,7 +158,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', help='Fix seed for reproducibility',type=int)
     parser.add_argument('--learning_rate', help='Learning rate to train model', type=float)
     parser.add_argument('--weight_decay', help='Amount of weight decay (L2 regularizer)', type=float)
-    parser.add_argument('--normalize', help='Normalize Type')
+    parser.add_argument('--normalize', help='norm_method Type')
     parser.add_argument('--mode', help='Mode to run, choose from (train), (val), (extract)',default='train')
     parser.add_argument('--eps', help="Adversarial attack strength")
 
@@ -165,4 +167,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     eps = args.eps.split("_")    
     save_folder = os.path.join(args.save_folder, 'vone_frontend')
+    seed_everything(args.seed)
     main(save_folder, args.model_name, args.seed, args.learning_rate, args.weight_decay, args.mode, eps, args.normalize)

code/vonenet/convnet_backend.py
@@ -37,7 +37,7 @@ class ConvNetBackEnd(nn.Module):
     def __init__(self, in_channels, normalize=None):
         super(ConvNetBackEnd, self).__init__()
 
-        self.conv_2 = nn.Conv2d(in_channels= in_channels, out_channels=20, kernel_size=5, stride=1)
+        self.conv_2 = nn.Conv2d(in_channels=in_channels, out_channels=20, kernel_size=5, stride=1)
         self.fc_1 = nn.Linear(in_features=500, out_features=10)
         self.relu = nn.ReLU()
 
@@ -64,11 +64,12 @@ class ConvNetBackEnd(nn.Module):
         self.normalize = normalize
 
     def forward(self, x):
+            # remove first layer
             # x = self.conv_1(x)
             # x = self.norm_dict1[self.normalize](x)
             # x = self.relu(x)
 
-            # same back-end
+            # same back-end, but remove norm layer
             x = self.conv_2(x)
             # x = self.norm_dict2[self.normalize](x)
             x = self.relu(x)

code/vonenet/modules.py
@@ -12,6 +12,36 @@ class Identity(nn.Module):
     def forward(self, x):
         return x
 
+class LRN(nn.Module):
+    def __init__(self, channel_size=1, spatial_size=1, alpha=1.0, beta=0.75, across_channel_spatial=True):
+        super(LRN, self).__init__()
+        self.across_channel_spatial = across_channel_spatial
+        self.spatial_pad = int((spatial_size-1.0)/2)
+        self.channel_pad = int((channel_size-1.0)/2)
+        if self.across_channel_spatial:
+            # AvgPool3d needs to have input shape (N, C, D, H, W)
+            self.average=nn.AvgPool3d(kernel_size=(channel_size, spatial_size, spatial_size),
+                    stride=1,
+                    padding=(self.channel_pad, self.spatial_pad, self.spatial_pad))
+        else: #if not, then only do LocalResponseNorm across spatial
+            self.average=nn.AvgPool2d(kernel_size=spatial_size,
+                    stride=1,
+                    padding=self.spatial_pad)
+        self.alpha = alpha
+        self.beta = beta
+
+
+    def forward(self, x):
+        if self.across_channel_spatial:
+            div = x.pow(2).unsqueeze(1) #squeeze to fit the input shape with AvgPool3d
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        x = x.div(div)
+        return x
 
 class GFB(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=4):
@@ -37,9 +67,13 @@ class GFB(nn.Module):
 
 
 class VOneBlock(nn.Module):
-    def __init__(self, sf, theta, sigx, sigy, phase,
-                 k_exc=7,
-                 simple_channels=16, complex_channels=16, ksize=7, stride=2, input_size=28):
+    def __init__(
+            self, sf, theta, sigx, sigy, phase,
+            k_exc=7,
+            simple_channels=16, complex_channels=16, ksize=7, stride=2, input_size=28,
+            norm_method='nn'
+        ):
+
         super().__init__()
 
         self.in_channels = 1
@@ -64,11 +98,35 @@ class VOneBlock(nn.Module):
         self.simple_conv_q1.initialize(sf=self.sf, theta=self.theta, sigx=self.sigx, sigy=self.sigy,
                                        phase=self.phase + np.pi / 2)
 
-        # self.simple = nn.ReLU(inplace=True)
         self.simple = nn.ReLU()
         self.complex = Identity()
         self.gabors = Identity()
         self.output = Identity()
+        
+        self.bn1 = nn.BatchNorm2d(self.out_channels)
+        self.bn2 = nn.BatchNorm2d(20)
+        self.ln1 = nn.LayerNorm([self.out_channels, 14, 14])
+        self.ln2 = nn.LayerNorm([20, 10, 10])
+        self.in1 = nn.InstanceNorm2d(self.out_channels, affine=True)
+        self.in2 = nn.InstanceNorm2d(20, affine=True)
+        self.gn1 = nn.GroupNorm(4, self.out_channels)
+        self.gn2 = nn.GroupNorm(4, 20)
+        self.lrn_channel = nn.LocalResponseNorm(5, alpha=0.001)
+        self.lrn_spatial = LRN(spatial_size=3, across_channel_spatial=False)
+        self.lrn_both = LRN(spatial_size=3, channel_size=5, across_channel_spatial=True)
+
+        self.norm_dict = {
+            'nn': nn.Identity(),
+            'bn': self.bn1, 
+            'ln': self.ln1, 
+            'in': self.in1, 
+            'gn': self.gn1, 
+            'lrns': self.lrn_spatial,
+            'lrnc': self.lrn_channel, 
+            'lrnb': self.lrn_both
+        }
+
+        self.norm_method = norm_method
 
     def gabors_f(self, x):
         s_q0 = self.simple_conv_q0(x)
@@ -82,6 +140,10 @@ class VOneBlock(nn.Module):
     def forward(self, x):
         # Gabor activations [Batch, out_channels, H/stride, W/stride]
         x = self.gabors_f(x)
+
+        # Normalization layer
+        x = self.norm_dict[self.norm_method](x)
+
         # V1 Block output: (Batch, out_channels, H/stride, W/stride)
         x = self.output(x)
         return x

code/vonenet/vonenet.py
@@ -11,7 +11,7 @@ import numpy as np
 def VOneNet(sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
             simple_channels=16, complex_channels=16,
             k_exc=7,
-            model_arch='convnet', image_size=28, visual_degrees=8, ksize=7, stride=2, normalize='nn'):
+            model_arch='convnet', image_size=28, visual_degrees=8, ksize=7, stride=2, norm_method='nn'):
 
     out_channels = simple_channels + complex_channels
 
@@ -32,10 +32,9 @@ def VOneNet(sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
     theta = theta/180 * np.pi
     phase = phase / 180 * np.pi
 
-    vone_block = VOneBlock(sf=sf, theta=theta, sigx=sigx, sigy=sigy, phase=phase,
-                           k_exc=k_exc,
+    vone_block = VOneBlock(sf=sf, theta=theta, sigx=sigx, sigy=sigy, phase=phase, k_exc=k_exc,
                            simple_channels=simple_channels, complex_channels=complex_channels,
-                           ksize=ksize, stride=stride, input_size=image_size)
+                           ksize=ksize, stride=stride, input_size=image_size, norm_method=norm_method)
 
     if model_arch:
         # bottleneck = nn.Conv2d(out_channels, 20, kernel_size=5, stride=1, bias=False)
@@ -52,7 +51,7 @@ def VOneNet(sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
             model_back_end = CORnetSBackEnd()
         elif model_arch.lower() == 'convnet':
             print('Model: ', 'VOneConvNet')
-            model_back_end = ConvNetBackEnd(in_channels=out_channels, normalize=normalize)
+            model_back_end = ConvNetBackEnd(in_channels=out_channels)
 
         model = nn.Sequential(OrderedDict([
             ('vone_block', vone_block),